e3d3a619f6f982ca63f07839a257381965d831f0
David GAILLETON <david.gailleton@posteo.net>
2025-07-24 15:17:10 +0200

    Player movement finish

--- Assets/Scripts/PlayerMouvement.cs
+++ Assets/Scripts/PlayerMouvement.cs
@@ -6,8 +6,8 @@ public class PlayerMouvement : MonoBehaviour
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float jumpHeight = 10f;
     [SerializeField] private float gravity = -9.81f;
-    [SerializeField] private float sensivity = 10f;
-    [SerializeField] private GameObject body;
+    [SerializeField] private float sensivity = 1f;
+    [SerializeField] private GameObject player;
     [SerializeField] private GameObject camera;
     
     
@@ -25,18 +25,40 @@ public class PlayerMouvement : MonoBehaviour
         float mouseX = Input.GetAxisRaw("Mouse X");
         float mouseY = Input.GetAxisRaw("Mouse Y");
         HandleMouseLook(mouseX, mouseY);
+        PlayerMovement();
     }
 
     private void HandleMouseLook(float mouseX, float mouseY)
     {
-        Vector3 currentRotation = body.transform.localEulerAngles;
-        currentRotation.y += mouseX;
+        Vector3 currentRotation = player.transform.localEulerAngles;
+        currentRotation.y += mouseX * sensivity;
         
-        body.transform.localRotation = Quaternion.AngleAxis(currentRotation.y, Vector3.up);
+        player.transform.localRotation = Quaternion.AngleAxis(currentRotation.y, Vector3.up);
         
         Vector3 currentCameraRotation = camera.gameObject.transform.localEulerAngles;
-        currentCameraRotation.x -= mouseY;
+        currentCameraRotation.x -= mouseY * sensivity;
         
         camera.gameObject.transform.localRotation = Quaternion.AngleAxis(currentCameraRotation.x, Vector3.right);
+    }
+
+    private void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            player.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        } 
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            player.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            player.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
     }
 }
